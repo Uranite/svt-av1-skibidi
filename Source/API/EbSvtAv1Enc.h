2f09f24ecb17fc6da686e77b81e7b973ff22f0a3
igorbaryshev <116152084+igorbaryshev@users.noreply.github.com>
2026-02-18 03:50:42 +0300

    Add built-in photon noise table generation (#28 - #33)

--- Source/API/EbSvtAv1Enc.h
+++ Source/API/EbSvtAv1Enc.h
@@ -1139,6 +1139,28 @@ typedef struct EbSvtAv1EncConfiguration {
      */
     uint8_t cdef_scaling;
 
+    /**
+     * @brief Photon noise ISO value
+     *
+     * Default is 0.
+     */
+    uint32_t photon_noise_iso;
+
+    /**
+     * @brief Enable chroma noise, which will is scaled based on luma values for generated film grain table.
+     *
+     * 0: disable chroma scaling
+     * 1: enable chroma scaling
+     *
+     * Default is 0.
+     */
+    uint8_t enable_photon_noise_chroma;
+
+    /**
+     * @brief Check if color range is provided by the user
+     */
+    bool color_range_provided;
+
     // clang-format off
     /* Add 128 Byte Padding to Struct to avoid changing the size of the public configuration struct */
     uint8_t padding[128
@@ -1149,6 +1171,9 @@ typedef struct EbSvtAv1EncConfiguration {
         - sizeof(bool) // enable_intrabc
         - sizeof(uint8_t) // max_managed_refs (ref-frame mgmt)
         - sizeof(uint8_t) // max_hierarchical_levels (runtime MG size change)
+        - sizeof(uint32_t) // photon_noise_iso
+        - sizeof(uint8_t) // enable_photon_noise_chroma
+        - sizeof(bool) // color_range_provided
     ];
     // clang-format on
 } EbSvtAv1EncConfiguration;
