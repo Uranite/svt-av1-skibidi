c5d3cbc42c27e502c9e624ea4c52d2cc38f77a0e
Uranite <62639703+Uranite@users.noreply.github.com>
2026-05-08 20:37:57 +0700

    Add alt-cdef from Essential

--- Source/API/EbSvtAv1Enc.h
+++ Source/API/EbSvtAv1Enc.h
@@ -1213,6 +1213,14 @@ typedef struct EbSvtAv1EncConfiguration {
     SvtAv1QualityZone* quality_zones;
     uint16_t           num_zones;
 
+    /**
+     * @brief Enable alternative CDEF biases
+     * 0: disabled
+     * 1-3: enabled
+     * Default is 0
+     */
+    uint8_t alt_cdef;
+
     // clang-format off
     /* Add 128 Byte Padding to Struct to avoid changing the size of the public configuration struct */
     uint8_t padding[128
@@ -1224,7 +1232,7 @@ typedef struct EbSvtAv1EncConfiguration {
         - sizeof(uint8_t) // max_managed_refs (ref-frame mgmt)
         - sizeof(uint8_t) // max_hierarchical_levels (runtime MG size change)
         /* SVT-AV1-HDR additions */
-        - (sizeof(uint8_t) * 10) - (sizeof(int8_t) * 1) - (sizeof(int32_t) * 2) - (sizeof(bool) * 4) - (sizeof(double)) -
+        - (sizeof(uint8_t) * 11) - (sizeof(int8_t) * 1) - (sizeof(int32_t) * 2) - (sizeof(bool) * 4) - (sizeof(double)) -
         sizeof(SvtAv1QualityZone*) - sizeof(uint16_t)];
     // clang-format on
 } EbSvtAv1EncConfiguration;
