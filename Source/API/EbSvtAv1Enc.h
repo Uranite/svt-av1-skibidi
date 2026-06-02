96a12aa2c9eaca7f93ca366ac0ef19eaa371e995
Uranite <62639703+Uranite@users.noreply.github.com>
2026-06-02 12:25:27 +0700

    Add low-memory parameter from Essential

--- Source/API/EbSvtAv1Enc.h
+++ Source/API/EbSvtAv1Enc.h
@@ -1232,6 +1232,12 @@ typedef struct EbSvtAv1EncConfiguration {
      */
     uint8_t enable_daala;
 
+    /* @brief use settings which reduce memory usage
+     *
+     * Default is false.
+     */
+    bool low_memory;
+
     // clang-format off
     /* Add 128 Byte Padding to Struct to avoid changing the size of the public configuration struct */
     uint8_t padding[128
@@ -1243,7 +1249,7 @@ typedef struct EbSvtAv1EncConfiguration {
         - sizeof(uint8_t) // max_managed_refs (ref-frame mgmt)
         - sizeof(uint8_t) // max_hierarchical_levels (runtime MG size change)
         /* SVT-AV1-HDR additions */
-        - (sizeof(uint8_t) * 12) - (sizeof(int8_t) * 1) - (sizeof(int32_t) * 2) - (sizeof(bool) * 4) - (sizeof(double)) -
+        - (sizeof(uint8_t) * 12) - (sizeof(int8_t) * 1) - (sizeof(int32_t) * 2) - (sizeof(bool) * 5) - (sizeof(double)) -
         sizeof(SvtAv1QualityZone*) - sizeof(uint16_t)];
     // clang-format on
 } EbSvtAv1EncConfiguration;
